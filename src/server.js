84309caf7b8673cd87873abad7b1dca1a9248cb5
nafifurqon <muhammadnafi98@gmail.com>
2022-03-17 08:26:58 +0700

    feat: changed ip to 0.0.0.0 for production

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.37.25',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
