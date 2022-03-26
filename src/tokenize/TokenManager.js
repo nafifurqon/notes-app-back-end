6a397939f66b7320a185e8d963fb468f21602d5d
nafifurqon <muhammadnafi98@gmail.com>
2022-03-26 19:13:16 +0700

    feat: Membuat Fungsi verifyRefreshToken

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -7,6 +7,19 @@ const TokenManager = {
   generateRefreshToken: (payload) => {
     return Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY);
   },
+  verifyRefreshToken: (refreshToken) => {
+    try {
+      const artifacts = Jwt.token.decode(refreshToken);
+
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
+
+      const {payload} = artifacts.decoded;
+
+      return payload;
+    } catch (error) {
+      throw new InvariantError('Refresh token tidak valid');
+    }
+  },
 };
 
 module.exports = TokenManager;
